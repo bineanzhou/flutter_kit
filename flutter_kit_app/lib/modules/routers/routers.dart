9b4a1b3c384d84c791a451ebfb507c989131bb37
bineanzhou <bineanzhou@163.com>
2019-10-21 15:36:18 +0800

    add RowPage

--- flutter_kit_app/lib/modules/routers/routers.dart
+++ flutter_kit_app/lib/modules/routers/routers.dart
@@ -2,12 +2,14 @@ import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_kit_app/modules/sample/sample_page.dart';
 import 'package:flutter_kit_app/modules/widgets/container_page.dart';
+import 'package:flutter_kit_app/modules/widgets/row_page.dart';
 
 class Routes {
   static String root = "/";
   static String home = "/home";
   static String samplePage = "lib/modules/sample/sample_page";
   static String containerPage = "lib/modules/widgets/container_page.dart";
+  static String rowPage = "lib/modules/widgets/row_page.dart";
 
   static void configureRoutes(Router router) {
     router.notFoundHandler = new Handler(
@@ -15,16 +17,23 @@ class Routes {
         handlerFunc:
             (BuildContext context, Map<String, List<String>> params) {});
 
-    router.define(samplePage, handler: Handler(
+    router.define(rowPage, handler: Handler(
       handlerFunc: (BuildContext context, Map<String, List<String>> params) {
-        return SamplePage();
+        return RowPage();
       },
     ));
+
     router.define(containerPage, handler: Handler(
       handlerFunc: (BuildContext context, Map<String, List<String>> params) {
         return ContainerPage();
       },
     ));
+    router.define(samplePage, handler: Handler(
+      handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+        return SamplePage();
+      },
+    ));
+
   }
 }
 
